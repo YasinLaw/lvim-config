a88a2bc353b965e128e94ddb405f13ced485327d
Yangxin Luo <yasinlaw@outlook.com>
2023-04-09 23:00:13 +0800

    set lsp

--- config.lua
+++ config.lua
@@ -10,9 +10,9 @@ vim.opt.relativenumber = true
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  -- pattern = "*.lua",
-  timeout = 1000,
+	enabled = true,
+	-- pattern = "*.lua",
+	timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -60,7 +60,15 @@ lvim.builtin.treesitter.auto_install = true
 -- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
+require("lvim.lsp.manager").setup("lua_ls", {
+	Lua = {
+		workspace = { checkThirdParty = false },
+		telemetry = { enable = false },
+		format = {
+			enable = false,
+		},
+	},
+})
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
@@ -79,23 +87,29 @@ lvim.builtin.treesitter.auto_install = true
 -- end
 
 -- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-width", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua" },
+
+	{ command = "clang-format", extra_args = { "--style=Google" } },
+
+	{ command = "autopep8" },
+
+	{ command = "gofumpt" },
+	{ command = "goimports" },
+	{ command = "goimports-reviser" },
+
+	{ command = "rustfmt" },
+})
+
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "cpplint" },
+
+	{ command = "golangci-lint" },
+
+	{ command = "pylint" },
+})
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 -- lvim.plugins = {
